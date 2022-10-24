431d2c51b0aeedb6822b3988c5414e173e6d24a6
unknown <axelnperez@hotmail.com>
2022-10-24 23:56:07 +0200

    Added loading component for Results view

--- my-first-react-app/src/views/Results/index.js
+++ my-first-react-app/src/views/Results/index.js
@@ -1,14 +1,16 @@
 import { useParams } from 'react-router-dom';
 import moviesImage from "../../assets/movie_theater.png";
 import { useFetchMoviesQuery } from '../../redux/api/movies';
+import Loading from './components/Loading';
 
 const Results = () => {
     const { title } = useParams();
-    const { data, error, isLoading } = useFetchMoviesQuery(title);
+    const { data, error, isLoading, isFetching } = useFetchMoviesQuery(title);
+
     return (
     <div className='flex flex-row'>
-        <div className='w-3/5'>
-
+        <div className='w-3/5 h-screen overflow-y-auto px-10'>
+            {isLoading && isFetching && <Loading/>}
         </div>
         <div className='w-2/5'>
             <img src={moviesImage} alt='Movies theater' className='w-full h-screen object-cover'/>
