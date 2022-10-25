255d06f03368d3d54c5ae99d711d90c2d5b4003f
unknown <axelnperez@hotmail.com>
2022-10-25 18:50:57 +0200

    Stylized lists of results in the results view

--- my-first-react-app/src/views/Results/components/List.js
+++ my-first-react-app/src/views/Results/components/List.js
@@ -1,13 +1,14 @@
 const List = ({ data }) => {
     return data?.map(movie => (
-        <div key={movie.imdbID}>
-            <div>
-                <img src={movie.Poster} alt={movie.Title} ></img>
+        <div key={movie.imdbID} className="flex flex-row w-full mt-16" style={{ minWidth: 650 }}>
+            <div className="w-1/6">
+                <img src={movie.Poster} alt={movie.Title} className="w-64"></img>
             </div>
-            <div>
-                <p>{movie.Title}</p>
-                <div>
-                    <button>
+            <div className="w-5/6 flex flex-col items-start py-1 px-4">
+                <p className="font-lato text-3xl">{movie.Title}</p>
+                <div className="flex h-full items-end">
+                    <button className="border-2 border-red-400 rounded-md 
+                    w-48 h-10 text-xl text-red-400 hover:bg-red-400 hover:text-white">
                         Ver mas
                     </button>
                 </div>
